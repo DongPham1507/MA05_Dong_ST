daf83ec2d957e2775ccdf70daf3cb4a38eaa8afb
DongPham1507 <phamhaikhanhdong1507@gmail.com>
2023-06-08 18:20:08 +0700

    commit bài tập về buổi 2

---
+++ Buoi_2/Baitap_vn.py
@@ -0,0 +1,9 @@
+First_name = "Đông"
+Last_name = "Phạm"
+age = "13"
+grade = "8"
+school = "AISVN"
+print("Ho và tên:", First_name, Last_name)
+print("Tuổi:", age)
+print("Lớp:", grade)
+print("trường:", school)
